d152668fcd117ac6dd69fc1e39b1dd316670f94d
sanket <sanketkopnar1299@gmail.com>
2024-01-18 10:55:13 +0530

    update header and show the user data in header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home/Home";
 import { Login } from "./Pages/Login/Login";
-import { Profile } from "./Pages/Profile/Profile";
 import { SignUp } from "./Pages/SignUp/SignUp";
 import { About } from "./Pages/About/About";
 import { Header } from "./Components/Header/Header";
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/about" element={<About />} />
         </Routes>
