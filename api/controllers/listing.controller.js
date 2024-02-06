9631daf640b475501a2b8b21f75ac1dee9aad7db
sanket <sanketkopnar1299@gmail.com>
2024-02-06 10:22:34 +0530

    user can delete listing functionality done

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import { Listing } from "../models/listing.models.js";
+import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asynchHandler.js";
 
@@ -16,4 +17,28 @@ const createListing = asyncHandler(async (req, res) => {
       console.log("error", error);
    }
 });
-export { createListing };
+
+// delete Listing
+const deleteUserListing = asyncHandler(async (req, res) => {
+   // console.log("reqparams", req.params.id);
+   // console.log("requser", req.user._id);
+   const userListngs = await Listing.findById(req.params.id);
+   console.log("user", userListngs);
+
+   if (!userListngs) {
+      throw new apiError(404, "Not Valid user or Listings not exist");
+   }
+
+   if (req.user._id.toString() !== userListngs.userRef) {
+      throw new apiError(404, "user is invalid");
+   }
+   try {
+      await Listing.findByIdAndDelete(req.params.id);
+      return res
+         .status(200)
+         .json(new apiError(200, "Succesfully Listing deleted"));
+   } catch (error) {
+      console.log("error exist at delete UserListing", error);
+   }
+});
+export { createListing, deleteUserListing };
